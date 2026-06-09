54ab37b6fb9e802ff4d930e366535dbe51667891
KOU050223 <kouhei20050223@icloud.com>
2026-06-09 20:36:36 +0900

    feat: flake.nixと関連ファイルを追加する

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Baka and Test: Summon the Beasts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pnpmPackage = pkgs.pnpm_10 or pkgs.pnpm;
+        goPackage = pkgs.go_1_26 or pkgs.go;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.git
+            pkgs.go-task
+
+            # frontend
+            pkgs.nodejs_24
+            pnpmPackage
+            
+            # Go
+            pkgs.golangci-lint
+            goPackage
+
+            # Ruby on Rails
+            pkgs.ruby_4_0
+            pkgs.bundler
+            pkgs.libyaml
+            pkgs.libxml2
+            pkgs.libxslt
+            pkgs.zlib
+            pkgs.openssl
+            pkgs.postgresql
+          ];
+
+          shellHook = ''
+            export GEM_HOME=$HOME/.gem
+            export PATH=$GEM_HOME/bin:$PATH
+          '';
+        };
+      });
+}
